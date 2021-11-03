a921e7373d4f4a913ca49ae67c3f557aa0ec08d1
narayanakreddy <narayanak1515@gmail.com>
2021-11-03 10:13:43 +0530

    port changed

--- app.js
+++ app.js
@@ -1,23 +1,19 @@
-var express = require('express'); //including express  npm
-var path = require('path');
+var express = require("express"); //including express  npm
+var path = require("path");
 var bodyParser = require("body-parser");
-var app = express(); // Creating instance   
-var port = 4000; // setting port for the application   
+var app = express(); // Creating instance
+var port = 6072; // setting port for the application
 
-var routes = require("./routes")
+var routes = require("./routes");
 
-app.set('port', process.env.PORT || 3000)
-app.use(express.static(path.join(__dirname, 'public')));
+app.set("port", port);
+app.use(express.static(path.join(__dirname, "public")));
 app.set("views", path.join(__dirname, "views"));
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-app.use(routes)
+app.use(routes);
 
 app.listen(app.get("port"), function () {
-    console.log('Your application is running on : ' + app.get("port") + ' port');
-
+  console.log("Your application is running on : " + app.get("port") + " port");
 });
-
-
-
